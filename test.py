d9dae40781273a2cbbe5d1d7598b113ad35bb033
lionel42 <43442120+lionel42@users.noreply.github.com>
2021-12-17 12:26:50 +0100

    change game

--- test.py
+++ test.py
@@ -2,5 +2,5 @@ from pysimgame.game_manager import GameManager
 
 if __name__ == "__main__":
     GAME_MANAGER = GameManager()
-    # GAME_MANAGER.start_new_game("2 Teacups")
-    GAME_MANAGER.start_new_game("asdf")
+    GAME_MANAGER.start_new_game("2 Teacups")
+    # GAME_MANAGER.start_new_game("asdf")
